c883d5072abf3701c84543e46c48654c8d9fbe13
Harshil Sanghavi <harshilsan7@gmail.com>
2023-04-13 20:32:30 -0400

    Debug

--- revenue.java
+++ revenue.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.Scanner;
+
 
 public class revenue {
 	// MariaDB Credentials
